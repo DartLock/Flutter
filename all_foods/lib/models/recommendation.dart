ccc1ed4e5423c4fb724aded68c084850e7a41a8c
Vitaly Simakov <leaflit@yandex.ru>
2025-03-09 17:52:43 +0500

    all_foods: Recommendations Section. ready

--- all_foods/lib/models/recommendation.dart
+++ all_foods/lib/models/recommendation.dart
@@ -1,20 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 class Recommendation {
-  String title;
-  String iconPath;
-  String level;
-  String duration;
-  String colorie;
+  Text title;
+  SvgPicture icon;
+  Text info;
   Color boxColor;
   bool isSelected;
 
   Recommendation({
     required this.title,
-    required this.iconPath,
-    required this.level,
-    required this.duration,
-    required this.colorie,
+    required this.icon,
+    required this.info,
     required this.isSelected,
     required this.boxColor,
   });
@@ -22,29 +19,37 @@ class Recommendation {
   static List<Recommendation> getRecommendation() {
     List<Recommendation> recommendations = [];
 
-    recommendations.add(
-        Recommendation(
-          title: 'Honey Pancake',
-          iconPath: 'assets/icons/body/recommendations/honey_pancake.svg',
-          level: 'Easy',
-          duration: '30mins',
-          colorie: '180kCal',
-          isSelected: true,
-          boxColor: const Color(0xFF9CD6FF),
-        )
+    SvgPicture honeyPancakeIcon = SvgPicture.asset('assets/icons/body/recommendations/honey_pancake.svg');
+    SvgPicture canaiBreadIcon = SvgPicture.asset('assets/icons/body/recommendations/canai_bread.svg');
+
+    TextStyle titleStyle = const TextStyle(fontWeight: FontWeight.w500, color: Colors.black, fontSize: 16,);
+    Text honeyPancakeTitle = Text(textAlign: TextAlign.center, 'Honey Pancake', style: titleStyle,);
+    Text canaiBreadTitle = Text(textAlign: TextAlign.center, 'Canai Bread', style: titleStyle,);
+
+    TextStyle infoStyle = const TextStyle(fontWeight: FontWeight.w500, color: Color(0xFF959595), fontSize: 13,);
+    Text honeyPancakeInfo = Text(textAlign: TextAlign.center, "Easy | 30mins | 180kCal", style: infoStyle,);
+    Text canaiBreadInfo = Text(textAlign: TextAlign.center, "Easy | 30mins | 180kCal", style: infoStyle,);
+
+    Color honeyBoxColor = const Color(0xFF9CD6FF);
+    Recommendation honeyPancakeRec = Recommendation(
+      title: honeyPancakeTitle,
+      icon: honeyPancakeIcon,
+      info: honeyPancakeInfo,
+      isSelected: true,
+      boxColor: honeyBoxColor,
     );
 
-    recommendations.add(
-        Recommendation(
-          title: 'Canai Bread',
-          iconPath: 'assets/icons/body/recommendations/canai_bread.svg',
-          level: 'Easy',
-          duration: '30mins',
-          colorie: '180kCal',
-          isSelected: true,
-          boxColor: const Color(0xFFE3D5FF),
-        )
+    Color breadBoxColor = const Color(0xFFE3D5FF);
+    Recommendation canaiBreadRec = Recommendation(
+      title: canaiBreadTitle,
+      icon: canaiBreadIcon,
+      info: canaiBreadInfo,
+      isSelected: true,
+      boxColor: breadBoxColor,
     );
+
+    recommendations.add(honeyPancakeRec);
+    recommendations.add(canaiBreadRec);
 
     return recommendations;
   }
--- all_foods/lib/pages/home/recommendations.dart
+++ all_foods/lib/pages/home/recommendations.dart
@@ -20,20 +20,12 @@ Column recommendations() {
         ),
       ),
       const SizedBox(height: 15,),
-      Container( // Category Main Row
-        height: 220,
-        // color: Colors.green,
+      SizedBox( // Category Main Row
+        height: 240,
         child: ListView.separated(
-          itemCount: recommendation.length,
-          scrollDirection: Axis.horizontal,
-          padding: const EdgeInsets.only(
-            left: 20,
-            right: 20,
-          ),
-          separatorBuilder: (context, index) => const SizedBox(width: 25,),
           itemBuilder: (context, index) { // Category Item Boxes
             return Container( // Category Item Box
-              width: 100,
+              width: 210,
               decoration: BoxDecoration(
                 color: recommendation[index].boxColor.withValues(alpha: 0.3),
                 borderRadius: BorderRadius.circular(16),
@@ -41,31 +33,20 @@ Column recommendations() {
               child: Column( // Category Child Sub Items
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  Container( // Category Child Sub Item
-                    width: 50,
-                    height: 50,
-                    decoration: const BoxDecoration(
-                      color: Colors.white,
-                      shape: BoxShape.circle,
-                    ),
-                    child: Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: SvgPicture.asset(recommendation[index].iconPath),
-                    ),
-                  ),
-                  Text( // Category Child Sub Item Title
-                    textAlign: TextAlign.center,
-                    recommendation[index].title,
-                    style: const TextStyle(
-                      fontWeight: FontWeight.w500,
-                      color: Colors.black,
-                      fontSize: 16,
-                    ),
-                  ),
+                  recommendation[index].icon,
+                  recommendation[index].title,
+                  recommendation[index].info,
                 ],
               ),
             );
           },
+          separatorBuilder: (context, index) => const SizedBox(width: 25,),
+          itemCount: recommendation.length,
+          scrollDirection: Axis.horizontal,
+          padding: const EdgeInsets.only(
+            left: 20,
+            right: 20,
+          ),
         ),
       ),
     ],
