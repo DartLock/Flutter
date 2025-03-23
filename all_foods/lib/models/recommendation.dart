61a4523efff709354432c013293766b1c439945d
Vitaly Simakov <vitaly.s.simakov@tele2.ru>
2025-03-23 15:43:55 +0500

    all_foods: Recommendations Section. refactor

--- all_foods/lib/models/recommendation.dart
+++ all_foods/lib/models/recommendation.dart
@@ -39,8 +39,8 @@ class Recommendation {
     ];
   }
 
-  static List<Recommendation> getRecommendation() {
-    List<Recommendation> recommendations = [];
+  static List<Container> getRecommendation() {
+    List<Container> recommendations = [];
     List<Color> listSequenceColors = [const Color(0xFF9CD6FF), const Color(0xFFE3D5FF)];
 
     TextStyle titleStyle = const TextStyle(fontWeight: FontWeight.w500, color: Colors.black, fontSize: 16,);
@@ -48,14 +48,14 @@ class Recommendation {
 
     List recommendationsData = _recommendationsData();
 
-    for (final (index, recommendationElement) in recommendationsData.indexed) {
-      SvgPicture icon = SvgPicture.asset(recommendationElement["asset"]);
-      Text title = Text(textAlign: TextAlign.center, recommendationElement["title"], style: titleStyle,);
-      Text info = Text(textAlign: TextAlign.center, recommendationElement["info"], style: infoStyle,);
+    for (final (index, recommendationDataElement) in recommendationsData.indexed) {
+      SvgPicture icon = SvgPicture.asset(recommendationDataElement["asset"]);
+      Text title = Text(textAlign: TextAlign.center, recommendationDataElement["title"], style: titleStyle,);
+      Text info = Text(textAlign: TextAlign.center, recommendationDataElement["info"], style: infoStyle,);
 
       int colorIndex = index % 2;
 
-      Recommendation recommendation = Recommendation(
+      Recommendation recommendationElement = Recommendation(
         title: title,
         icon: icon,
         info: info,
@@ -63,9 +63,56 @@ class Recommendation {
         boxColor: listSequenceColors[colorIndex],
       );
 
-      recommendations.add(recommendation);
+      Container recommendationContainer = _buildContainer(recommendationElement);
+
+      recommendations.add(recommendationContainer);
     }
 
     return recommendations;
+  }
+
+  static Container _buildContainer(recommendation) {
+    return Container( // Category Item Box
+      width: 210,
+      decoration: BoxDecoration(
+        color: recommendation.boxColor.withValues(alpha: 0.3),
+        borderRadius: BorderRadius.circular(16),
+      ),
+      child: Column( // Category Child Sub Items
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          recommendation.icon,
+          Column(children: [recommendation.title, recommendation.info,],),
+          _buttonView(recommendation, recommendation.isSelected),
+        ],
+      ),
+    );
+  }
+
+  static Container _buttonView(recommendation, isSelected) {
+    return Container( // Button View
+      height: 45,
+      width: 130,
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            isSelected ? const Color(0xFF9DCEFF) : Colors.transparent,
+            isSelected ? const Color(0xFF92A3FD) : Colors.transparent,
+          ],
+        ),
+        borderRadius: BorderRadius.circular(16),
+      ),
+      child: Center(
+        child: Text(
+          textAlign: TextAlign.center,
+          'View',
+          style: TextStyle(
+            fontWeight: FontWeight.w600,
+            color: isSelected ? const Color(0xFFCEE9FF) : Colors.transparent,
+            fontSize: 14,
+          ),
+        ),
+      ),
+    );
   }
 }
--- all_foods/lib/pages/home/recommendations.dart
+++ all_foods/lib/pages/home/recommendations.dart
@@ -3,34 +3,7 @@ import 'package:flutter_svg/svg.dart';
 import 'package:all_foods/models/recommendation.dart';
 
 Column recommendations() {
-  List<Recommendation> recommendation = Recommendation.getRecommendation();
-
-  Container _buttonView(recommendation) {
-    return Container( // Button View
-      height: 45,
-      width: 130,
-      decoration: BoxDecoration(
-        gradient: LinearGradient(
-          colors: [
-            recommendation.isSelected ? Color(0xFF9DCEFF) : Colors.transparent,
-            recommendation.isSelected ? Color(0xFF92A3FD) : Colors.transparent,
-          ],
-        ),
-        borderRadius: BorderRadius.circular(16),
-      ),
-      child: Center(
-        child: Text(
-          textAlign: TextAlign.center,
-          'View',
-          style: TextStyle(
-            fontWeight: FontWeight.w600,
-            color: recommendation.isSelected ? Color(0xFFCEE9FF) : Colors.transparent,
-            fontSize: 14,
-          ),
-        ),
-      ),
-    );
-  }
+  List<Container> recommendations = Recommendation.getRecommendation();
 
   return Column( // Category
     crossAxisAlignment: CrossAxisAlignment.start,
@@ -51,34 +24,12 @@ Column recommendations() {
         height: 240,
         child: ListView.separated(
           itemBuilder: (context, index) { // Category Item Boxes
-            return Container( // Category Item Box
-              width: 210,
-              decoration: BoxDecoration(
-                color: recommendation[index].boxColor.withValues(alpha: 0.3),
-                borderRadius: BorderRadius.circular(16),
-              ),
-              child: Column( // Category Child Sub Items
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  recommendation[index].icon,
-                  Column(
-                    children: [
-                      recommendation[index].title,
-                      recommendation[index].info,
-                    ],
-                  ),
-                  _buttonView(recommendation[index]),
-                ],
-              ),
-            );
+            return recommendations[index];
           },
           separatorBuilder: (context, index) => const SizedBox(width: 25,),
-          itemCount: recommendation.length,
+          itemCount: recommendations.length,
           scrollDirection: Axis.horizontal,
-          padding: const EdgeInsets.only(
-            left: 20,
-            right: 20,
-          ),
+          padding: const EdgeInsets.only(left: 20, right: 20,),
         ),
       ),
     ],
