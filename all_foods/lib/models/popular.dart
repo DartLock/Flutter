1beb8524fddddd8395739708b0217b68e74d2fbb
Vitaly Simakov <vitaly.s.simakov@tele2.ru>
2025-03-23 14:27:57 +0500

    all_foods: Popular Section. Adding. #3. Refactor

--- all_foods/lib/models/popular.dart
+++ all_foods/lib/models/popular.dart
@@ -7,28 +7,34 @@ class Popular {
   Color boxColor;
   bool active;
 
-  Popular({
-    required this.name,
-    required this.icon,
-    required this.boxColor,
-    this.active = false,
-  });
+  Popular({required this.name, required this.icon, required this.boxColor, this.active = false,});
+
+  static List<Map<String, String>> _popularData() {
+    return [
+      {"key": 'blueberry_pancake', "name": 'Blueberry Pancake', "asset": 'assets/icons/body/popular/blueberry_pancake.svg'},
+      {"key": 'lowfat_milk', "name": 'Lowfat Milk', "asset": 'assets/icons/body/popular/lowfat_milk.svg'},
+      {"key": 'salmon_nigiri', "name": 'Salmon Nigiri', "asset": 'assets/icons/body/popular/salmon_nigiri.svg'},
+      {"key": 'salad', "name": 'Salad', "asset": 'assets/icons/body/popular/salad.svg'},
+    ];
+  }
+
+  static Map getPopularByKey(String key) {
+    List<Map<String, String>> popularData = _popularData();
+
+    return popularData.firstWhere((element) => element["key"] == key);
+  }
 
   static List<Popular> getPopular() {
     List<Popular> popular = [];
     List<Color> listSequenceColors = [const Color(0xFF9CD6FF), const Color(0xFFE3D5FF)];
 
-    SvgPicture saladIcon = SvgPicture.asset('assets/icons/body/popular/blueberry_pancake.svg');
-    SvgPicture cakeIcon = SvgPicture.asset('assets/icons/body/popular/lowfat_milk.svg');
-    SvgPicture pieIcon = SvgPicture.asset('assets/icons/body/popular/salmon_nigiri.svg');
+    List popularData = _popularData();
 
-    Popular saladPopular = Popular(name: 'Salad', icon: saladIcon, boxColor: listSequenceColors[0],);
-    Popular cakePopular = Popular(name: 'Cake', icon: cakeIcon, boxColor: listSequenceColors[1],);
-    Popular piePopular = Popular(name: 'Pie', icon: pieIcon, boxColor: listSequenceColors[0],);
-
-    popular.add(saladPopular);
-    popular.add(cakePopular);
-    popular.add(piePopular);
+    for (final (index, popularElement) in popularData.indexed) {
+      SvgPicture icon = SvgPicture.asset(popularElement["asset"]);
+      int colorIndex = index % 2;
+      popular.add(Popular(name: popularElement["name"], icon: icon, boxColor: listSequenceColors[colorIndex],));
+    }
 
     return popular;
   }
