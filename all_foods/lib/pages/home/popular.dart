ace6402b3b37d4229685444157193afb10ecc18d
Vitaly Simakov <vitaly.s.simakov@tele2.ru>
2025-03-23 12:02:46 +0500

    all_foods: Popular Section. Adding. #2

--- all_foods/lib/pages/home/popular.dart
+++ all_foods/lib/pages/home/popular.dart
@@ -2,20 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:all_foods/models/popular.dart';
 
 Column popular() {
-  List<Popular> categories = Popular.getPopular();
+  List<Popular> popular = Popular.getPopular();
   Color colorBlack = Colors.black;
   Color colorWhite = Colors.white;
 
   Padding popularTitle = Padding( // Popular Title
     padding: const EdgeInsets.only(top: 40, left: 20,),
-    child: Text(
-      'Popular',
-      style: TextStyle(
-        color: colorBlack,
-        fontSize: 18,
-        fontWeight: FontWeight.w600,
-      ),
-    ),
+    child: Text('Popular', style: TextStyle(color: colorBlack, fontSize: 18, fontWeight: FontWeight.w600,),),
   );
 
   return Column( // Popular
@@ -27,18 +20,15 @@ Column popular() {
         height: 120,
         // color: Colors.green,
         child: ListView.separated(
-          itemCount: categories.length,
+          itemCount: popular.length,
           scrollDirection: Axis.vertical,
-          padding: const EdgeInsets.only(
-            left: 20,
-            right: 20,
-          ),
+          padding: const EdgeInsets.only(left: 20, right: 20,),
           separatorBuilder: (context, index) => const SizedBox(width: 25,),
           itemBuilder: (context, index) { // Popular Item Boxes
             return Container( // Popular Item Box
               // width: 100,
               decoration: BoxDecoration(
-                color: categories[index].boxColor.withValues(alpha: 0.3),
+                color: popular[index].boxColor.withValues(alpha: 0.3),
                 borderRadius: BorderRadius.circular(16),
               ),
               child: Column( // Popular Child Sub Items
@@ -47,18 +37,12 @@ Column popular() {
                   Container( // Popular Child Sub Item
                     width: 50,
                     height: 50,
-                    decoration: BoxDecoration(
-                      color: colorWhite,
-                      shape: BoxShape.circle,
-                    ),
-                    child: Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: categories[index].icon,
-                    ),
+                    decoration: BoxDecoration(color: colorWhite, shape: BoxShape.circle,), // Icon border style
+                    child: Padding(padding: const EdgeInsets.all(8.0), child: popular[index].icon,), // Icon imaging
                   ),
                   Text( // Popular Child Sub Item Title
                     textAlign: TextAlign.center,
-                    categories[index].name,
+                    popular[index].name,
                     style: TextStyle(
                       fontWeight: FontWeight.w400,
                       color: colorBlack,
