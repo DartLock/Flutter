1d71ed024c1e6b4432dd4e9c517a36a2c6f4bed5
Vitaly Simakov <vitaly.s.simakov@tele2.ru>
2025-03-23 15:02:04 +0500

    all_foods: Recommendations Section. refactor

--- all_foods/lib/models/recommendation.dart
+++ all_foods/lib/models/recommendation.dart
@@ -16,52 +16,55 @@ class Recommendation {
     required this.boxColor,
   });
 
+  static List<Map<String, String>> _recommendationsData() {
+    return [
+      {
+        "key": 'honey_pancake',
+        "title": 'Honey Pancake',
+        "info": "Easy | 30mins | 180kCal",
+        "asset": 'assets/icons/body/recommendations/honey_pancake.svg'
+      },
+      {
+        "key": 'canai_bread',
+        "title": 'Canai Bread',
+        "info": "Easy | 30mins | 180kCal",
+        "asset": 'assets/icons/body/recommendations/canai_bread.svg'
+      },
+      {
+        "key": 'fruit_cake',
+        "title": 'Fruit Cake',
+        "info": "Easy | 30mins | 180kCal",
+        "asset": 'assets/icons/body/recommendations/fruit_cake.svg'
+      },
+    ];
+  }
+
   static List<Recommendation> getRecommendation() {
     List<Recommendation> recommendations = [];
     List<Color> listSequenceColors = [const Color(0xFF9CD6FF), const Color(0xFFE3D5FF)];
 
-    SvgPicture honeyPancakeIcon = SvgPicture.asset('assets/icons/body/recommendations/honey_pancake.svg');
-    SvgPicture canaiBreadIcon = SvgPicture.asset('assets/icons/body/recommendations/canai_bread.svg');
-    SvgPicture fruitCakeIcon = SvgPicture.asset('assets/icons/body/recommendations/fruit_cake.svg');
-
     TextStyle titleStyle = const TextStyle(fontWeight: FontWeight.w500, color: Colors.black, fontSize: 16,);
     TextStyle infoStyle = const TextStyle(fontWeight: FontWeight.w500, color: Color(0xFF959595), fontSize: 13,);
 
-    Text honeyPancakeTitle = Text(textAlign: TextAlign.center, 'Honey Pancake', style: titleStyle,);
-    Text canaiBreadTitle = Text(textAlign: TextAlign.center, 'Canai Bread', style: titleStyle,);
-    Text fruitCakeTitle = Text(textAlign: TextAlign.center, 'Fruit Cake', style: titleStyle,);
+    List recommendationsData = _recommendationsData();
 
-    Text honeyPancakeInfo = Text(textAlign: TextAlign.center, "Easy | 30mins | 180kCal", style: infoStyle,);
-    Text canaiBreadInfo = Text(textAlign: TextAlign.center, "Easy | 30mins | 180kCal", style: infoStyle,);
-    Text fruitCakeInfo = Text(textAlign: TextAlign.center, "Easy | 30mins | 180kCal", style: infoStyle,);
+    for (final (index, recommendationElement) in recommendationsData.indexed) {
+      SvgPicture icon = SvgPicture.asset(recommendationElement["asset"]);
+      Text title = Text(textAlign: TextAlign.center, recommendationElement["title"], style: titleStyle,);
+      Text info = Text(textAlign: TextAlign.center, recommendationElement["info"], style: infoStyle,);
 
-    Recommendation honeyPancakeRec = Recommendation(
-      title: honeyPancakeTitle,
-      icon: honeyPancakeIcon,
-      info: honeyPancakeInfo,
-      isSelected: true,
-      boxColor: listSequenceColors[0],
-    );
+      int colorIndex = index % 2;
 
-    Recommendation canaiBreadRec = Recommendation(
-      title: canaiBreadTitle,
-      icon: canaiBreadIcon,
-      info: canaiBreadInfo,
-      isSelected: true,
-      boxColor: listSequenceColors[1],
-    );
+      Recommendation recommendation = Recommendation(
+        title: title,
+        icon: icon,
+        info: info,
+        isSelected: true,
+        boxColor: listSequenceColors[colorIndex],
+      );
 
-    Recommendation fruitCakeRec = Recommendation(
-      title: fruitCakeTitle,
-      icon: fruitCakeIcon,
-      info: fruitCakeInfo,
-      isSelected: true,
-      boxColor: listSequenceColors[0],
-    );
-
-    recommendations.add(honeyPancakeRec);
-    recommendations.add(canaiBreadRec);
-    recommendations.add(fruitCakeRec);
+      recommendations.add(recommendation);
+    }
 
     return recommendations;
   }
