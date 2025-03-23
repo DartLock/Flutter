5801fc60f3627d87dafb39d3387ec7f85aca0422
Vitaly Simakov <vitaly.s.simakov@tele2.ru>
2025-03-23 13:29:34 +0500

    all_foods: Categoriers. Has added element.

--- all_foods/lib/models/category.dart
+++ all_foods/lib/models/category.dart
@@ -17,19 +17,22 @@ class Category {
     List<Color> listSequenceColors = [const Color(0xFF9CD6FF), const Color(0xFFE3D5FF)];
 
     SvgPicture saladIcon = SvgPicture.asset('assets/icons/body/categories/salad.svg');
-    SvgPicture cakeIcon = SvgPicture.asset('assets/icons/body/categories/cake.svg');
+    SvgPicture oatmealIcon = SvgPicture.asset('assets/icons/body/categories/oatmeal.svg');
+    SvgPicture canaiBreadIcon = SvgPicture.asset('assets/icons/body/categories/canai_bread.svg');
     SvgPicture pieIcon = SvgPicture.asset('assets/icons/body/categories/pie.svg');
     SvgPicture pancakeIcon = SvgPicture.asset('assets/icons/body/categories/pancake.svg');
     SvgPicture smoothieIcon = SvgPicture.asset('assets/icons/body/categories/smoothie.svg');
 
     Category saladCategory = Category(name: 'Salad', icon: saladIcon, boxColor: listSequenceColors[0],);
-    Category cakeCategory = Category(name: 'Cake', icon: cakeIcon, boxColor: listSequenceColors[1],);
-    Category pieCategory = Category(name: 'Pie', icon: pieIcon, boxColor: listSequenceColors[0],);
-    Category pancakeCategory = Category(name: 'Pancake', icon: pancakeIcon, boxColor: listSequenceColors[1],);
-    Category smoothieCategory = Category(name: 'Smoothie', icon: smoothieIcon, boxColor: listSequenceColors[0],);
+    Category oatmealCategory = Category(name: 'Cake', icon: oatmealIcon, boxColor: listSequenceColors[1],);
+    Category canaiBreadCategory = Category(name: 'Cake', icon: canaiBreadIcon, boxColor: listSequenceColors[0],);
+    Category pieCategory = Category(name: 'Pie', icon: pieIcon, boxColor: listSequenceColors[1],);
+    Category pancakeCategory = Category(name: 'Pancake', icon: pancakeIcon, boxColor: listSequenceColors[0],);
+    Category smoothieCategory = Category(name: 'Smoothie', icon: smoothieIcon, boxColor: listSequenceColors[1],);
 
     categories.add(saladCategory);
-    categories.add(cakeCategory);
+    categories.add(oatmealCategory);
+    categories.add(canaiBreadCategory);
     categories.add(pieCategory);
     categories.add(pancakeCategory);
     categories.add(smoothieCategory);
